4f050edd7e7db893f52e71afd93796997c745507
Administrator <Administrator@PCOS-01121740>
2014-08-14 23:31:28 +0800

    添加HelloWorldAction，测试简单的struts流程

---
+++ basic-struts/src/main/java/org/teng/struts/basic/action/HelloWorldAction.java
@@ -0,0 +1,25 @@
+package org.teng.struts.basic.action;
+
+import org.teng.struts.basic.model.MessageStore;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloWorldAction extends ActionSupport {
+	 private static final long serialVersionUID = 1L;
+	 
+	    private MessageStore messageStore;
+	     
+	    public String execute() throws Exception {
+	         
+	        messageStore = new MessageStore() ;
+	        return SUCCESS;
+	    }
+	 
+	    public MessageStore getMessageStore() {
+	        return messageStore;
+	    }
+	 
+	    public void setMessageStore(MessageStore messageStore) {
+	        this.messageStore = messageStore;
+	    }
+}
