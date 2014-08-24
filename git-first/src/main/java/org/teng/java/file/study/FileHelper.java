1346b022e8ad5093529affe353e2d7b44aa6f2de
Administrator <Administrator@PCOS-01121740>
2014-08-24 21:44:33 +0800

    continue和break带标签方式测试

--- git-first/src/main/java/org/teng/java/file/study/FileHelper.java
+++ git-first/src/main/java/org/teng/java/file/study/FileHelper.java
@@ -1,8 +1,11 @@
 package org.teng.java.file.study;
 
+import org.apache.log4j.Logger;
+
 public class FileHelper {
-	
+	private static Logger logger = Logger.getLogger(FileHelperTest.class); 
 	public static void main(String[] args){
-		System.out.println();
+		logger.info("app start");
+		logger.error("app start", new Exception("app exception"));
 	}
 }
--- git-first/src/main/java/org/teng/java/study/basic/BasicStudy.java
+++ git-first/src/main/java/org/teng/java/study/basic/BasicStudy.java
@@ -1,8 +1,15 @@
 package org.teng.java.study.basic;
 
 import java.util.Arrays;
-
-public class BasicStudy {
+class BasicStudy {
+	
+	
+	
+	/**
+	 * instanceof类型判断测试
+	 * 父对象instanceof子类：false
+	 * 子对象instanceof父类：true
+	 */
 	public static void instanceofTest() {
 
         Parent obj1 = new Parent();
@@ -22,6 +29,9 @@ public class BasicStudy {
             + (obj2 instanceof MyInterface));
     }
 	
+	/**
+	 * Arrays类提供了array操作的排序、查找、填充、复制等功能
+	 */
 	public static void arrayMethodsTest(){
 		char[] copyFrom = { 'd', 'e', 'c', 'a', 'f', 'f', 'e', 'i', 'n', 'a',
 				't', 'e', 'd' };
@@ -50,13 +60,125 @@ public class BasicStudy {
 	}
 	
 	
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		//arrayMethodsTest();
-		instanceofTest();
+		//instanceofTest();
+		/*测试块执行顺序*/
+		ExcuteOrder eo = new ExcuteOrder();
+		eo.continueTest();
+		eo.breakTest();
 	}	
 }
 
+//测试用到的类
 
 class Parent {}
 class Child extends Parent implements MyInterface {}
 interface MyInterface {}
+/**
+ * 测试一个类的执行顺序
+ * @author Administrator
+ *
+ */
+class ExcuteOrder{
+	/**
+	 * 测试构造函数和块的执行顺序
+	 * 在类中，块的执行要比构造早##
+	 */
+	static{
+		System.out.println("this is a static block_1");
+	}
+	
+	{
+		System.out.println("this is a non-static block_1");
+	}
+	
+	static{
+		System.out.println("this is a static block_2");
+	}
+	
+	public ExcuteOrder(){
+		System.out.println("this is construct function");
+		{
+			System.out.println("....");
+		}
+	}
+	
+	{
+		System.out.println("this is a non-static block_2");
+	}
+	
+	static{
+		System.out.println("this is a static block_3");
+	}
+	
+	/**
+	 * continue的特殊用法：双层循环跳出外层
+	 */
+	public void continueTest(){
+		System.out.println("=========================特殊的continue测试=========================");
+		 String searchMe = "Look for a substring in me";
+	        String substring = "sub";
+	        boolean foundIt = false;
+
+	        int max = searchMe.length() - 
+	                  substring.length();
+
+	    test:
+	        for (int i = 0; i <= max; i++) {
+	            int n = substring.length();
+	            int j = i;
+	            int k = 0;
+	            while (n-- != 0) {
+	                if (searchMe.charAt(j++) != substring.charAt(k++)) {
+	                    continue test;
+	                }
+	            }
+	            foundIt = true;
+	                break test;
+	        }
+	        System.out.println(foundIt ? "Found it" : "Didn't find it");
+	    }
+	
+	
+	/**
+	 * break的特殊用法
+	 */
+	public void breakTest(){
+		System.out.println("=========================特殊的break测试=========================");
+
+	      int[][] arrayOfInts = { 
+	              { 32, 87, 3, 589 },
+	              { 12, 1076, 2000, 8 },
+	              { 622, 127, 77, 955 }
+	          };
+	          int searchfor = 12;
+
+	          int i;
+	          int j = 0;
+	          boolean foundIt = false;
+
+	      search:
+	          for (i = 0; i < arrayOfInts.length; i++) {
+	              for (j = 0; j < arrayOfInts[i].length;
+	                   j++) {
+	                  if (arrayOfInts[i][j] == searchfor) {
+	                      foundIt = true;
+	                      break search;
+	                  }
+	              }
+	          }
+
+	          if (foundIt) {
+	              System.out.println("Found " + searchfor + " at " + i + ", " + j);
+	          } else {
+	              System.out.println(searchfor + " not in the array");
+	          }
+	}
+	
+}
+
+
