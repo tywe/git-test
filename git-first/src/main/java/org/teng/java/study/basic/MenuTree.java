a916184ff2bd4c55179a1a40dcbec5b92b250882
Administrator <Administrator@PCOS-01121740>
2014-08-31 17:57:47 +0800

    提交变化

--- git-first/src/main/java/org/teng/java/study/basic/MenuTree.java
+++ git-first/src/main/java/org/teng/java/study/basic/MenuTree.java
@@ -8,86 +8,112 @@ import java.util.TreeMap;
 
 import org.apache.log4j.Logger;
 
+
+/**
+ * @author Administrator
+ *
+ */
+/**
+ * @author Administrator
+ *
+ */
 public class MenuTree {
 	private static Logger logger = Logger.getLogger(MenuTree.class);
 	public List<Map<String, Object>> treeNodes;
-	public MenuTree(){
+	private static String string;
+
+	public MenuTree() {
 		treeNodes = new ArrayList<Map<String, Object>>();
 		initTreeNodes();
 	}
-	
-	public void initTreeNodes(){
-		//一级菜单
+
+	public void initTreeNodes() {
+		// 一级菜单
 		Map<String, Object> treeNode11 = new HashMap<String, Object>();
 		treeNode11.put("menuId", 1);
 		treeNode11.put("pMenuId", 0);
 		treeNode11.put("isLeaf", 0);
 		treeNodes.add(treeNode11);
-		
-		//二级菜单
+
+		// 二级菜单
 		Map<String, Object> treeNode21 = new HashMap<String, Object>();
 		treeNode21.put("menuId", 2);
 		treeNode21.put("pMenuId", 1);
 		treeNode21.put("isLeaf", 0);
 		treeNodes.add(treeNode21);
-		
+
 		Map<String, Object> treeNode22 = new HashMap<String, Object>();
 		treeNode22.put("menuId", 3);
 		treeNode22.put("pMenuId", 1);
 		treeNode22.put("isLeaf", 1);
 		treeNodes.add(treeNode22);
-		
-		//三级菜单
+
+		// 三级菜单
 		Map<String, Object> treeNode31 = new HashMap<String, Object>();
 		treeNode31.put("menuId", 4);
 		treeNode31.put("pMenuId", 2);
 		treeNode31.put("isLeaf", 1);
 		treeNodes.add(treeNode31);
-		
+
 		Map<String, Object> treeNode32 = new HashMap<String, Object>();
 		treeNode32.put("menuId", 5);
 		treeNode32.put("pMenuId", 2);
 		treeNode32.put("isLeaf", 1);
 		treeNodes.add(treeNode32);
-		
+
 	}
-	
-	public List<Map<String, Object>> searchLeafMenus(List<Map<String, Object>> menuList){
-		if(null == menuList || menuList.size() == 0){
-			logger.error("menuList is null in search leaf", new Exception("null pointer exception"));
+
+	/**
+	 * @param menuList
+	 * @return
+	 */
+	public List<Map<String, Object>> searchLeafMenus(
+			List<Map<String, Object>> menuList) {
+		if (null == menuList || menuList.size() == 0) {
+			logger.error("menuList is null in search leaf", new Exception(
+					"null pointer exception"));
 			return null;
 		}
 		List<Map<String, Object>> leafMenus = new ArrayList<Map<String, Object>>();
-		for(Map<String, Object> menu : menuList){
-			int isLeaf = (Integer)menu.get("isLeaf");
-			
-			if(isLeaf == 1){
+		for (Map<String, Object> menu : menuList) {
+			int isLeaf = (Integer) menu.get("isLeaf");
+
+			if (isLeaf == 1) {
 				leafMenus.add(menu);
 			}
 		}
-		
+
 		return leafMenus;
 	}
+
 	
-	public Map<Integer, Object> transMenuType(List<Map<String, Object>> menuList){
-		if(null == menuList || menuList.size() == 0){
-			logger.error("menuList is null in transMenuType", new Exception("null pointer exception"));
+	/**
+	 * @param menuList
+	 * @return
+	 */
+	public Map<Integer, Object> transMenuType(List<Map<String, Object>> menuList) {
+		if (null == menuList || menuList.size() == 0) {
+			logger.error("menuList is null in transMenuType", new Exception(
+					"null pointer exception"));
 			return null;
 		}
-		
+
 		Map<Integer, Object> idMenuMap = new TreeMap<Integer, Object>();
-		
-		for(Map<String, Object> menu : menuList){
+
+		for (Map<String, Object> menu : menuList) {
 			Integer menuId = (Integer) menu.get("menuId");
-			
+
 			idMenuMap.put(menuId, menu);
 		}
-		
+
 		return idMenuMap;
 	}
-	
-	
-	public static void main(String[] args){
-		
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		string = "...........）；";
+		System.out.println(string);
 	}
 }
